3974d875c5f69445fb629de6f92616006732f637
Ghasem Kiani <ghasemkiani@gmail.com>
2026-05-01 15:59:19 +0330

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ class App extends cutil.mixin(AppBase, infoer, dumper, pathable, iwdbApp) {
 				...infoer.defaultPrefsInfoer,
 				...iwdbApp.defaultPrefsIWDbApp,
 			},
-      sqlCreate: `@create.sql`,
+			sqlCreate: `@create.sql`,
 			_client: null,
 		});
 	}
@@ -51,7 +51,7 @@ class App extends cutil.mixin(AppBase, infoer, dumper, pathable, iwdbApp) {
 		app.commander.command("do")
 			.alias("create")
 			.alias("c")
-      .description("Shorten a URL")
+			.description("Shorten a URL")
 			.argument("<url>", "url to shorten")
 			.option("-i, --shortid <shortid>", "shortid")
 			.option("-a, --analytics", "opt in for analytics (default: true)")
@@ -93,8 +93,8 @@ class App extends cutil.mixin(AppBase, infoer, dumper, pathable, iwdbApp) {
 			.command("list")
 			.alias("l")
 			.description("list all urls")
-      .option("-o, --oldest", "sort by date oldest first")
-      .option("-n, --newest", "sort by date newest first")
+			.option("-o, --oldest", "sort by date oldest first")
+			.option("-n, --newest", "sort by date newest first")
 			.action(async ({ oldest, newest }) => {
 				app.sub("run", async () => {
 					await app.toList({ oldest, newest });
@@ -110,36 +110,42 @@ class App extends cutil.mixin(AppBase, infoer, dumper, pathable, iwdbApp) {
 					await app.toSearch({ query });
 				});
 			});
-    app.commander
-      .command("update")
-      .alias("edit")
-      .alias("u")
-      .alias("e")
-      .description("edit and update an item from db")
-      .argument("<id>", "item id")
-      .action(async (id) => {
-        app.sub("run", async () => {
-          await app.toUpdate({ id });
-        });
-      });
-    app.commander
-      .command("export")
-      .description("export database to json file")
-      .argument("<file>", "path to output json file")
-      .action(async (file) => {
-        app.sub("run", async () => {
-          await app.toExport({ file });
-        });
-      });
-    app.commander
-      .command("import")
-      .description("import database from json file")
-      .argument("<file>", "path to input json file")
-      .action(async (file) => {
-        app.sub("run", async () => {
-          await app.toImport({ file });
-        });
-      });
+		app.commander
+			.command("update")
+			.alias("edit")
+			.alias("u")
+			.alias("e")
+			.description("edit and update an item from db")
+			.argument("<id>", "item id")
+			.action(async (id) => {
+				app.sub("run", async () => {
+					await app.toUpdate({ id });
+				});
+			});
+		app.commander
+			.command("export")
+			.description("export database to json file")
+			.argument("<file>", "path to output json file")
+			.action(async (file) => {
+				app.sub("run", async () => {
+					await app.toExport({ file });
+				});
+			});
+		app.commander
+			.command("import")
+			.description("import database from json file")
+			.argument("<file>", "path to input json file")
+			// Adding the merge option
+			.option(
+				"-m, --merge",
+				"merge records by discarding IDs from the file",
+				false,
+			)
+			.action(async (file, { merge }) => {
+				app.sub("run", async () => {
+					await app.toImport({ file, merge });
+				});
+			});
 	}
 	async toApplyInitOptions() {
 		await super.toApplyInitOptions();
@@ -161,50 +167,50 @@ class App extends cutil.mixin(AppBase, infoer, dumper, pathable, iwdbApp) {
 		// await app.toShowInfoInfoer();
 		await app.toShowInfoIWDbApp();
 	}
-  async toAddToDb({ url, u }) {
-    let app = this;
-    let id;
-    try {
-      let stmt = app.db.prepare(`
+	async toAddToDb({ url, u }) {
+		let app = this;
+		let id;
+		try {
+			let stmt = app.db.prepare(`
         INSERT INTO urls (url, u) 
         VALUES (?, ?)
       `);
-      let result = stmt.run(url, u);
-      id = result.lastInsertRowid;
-      console.log(`db id: ${id}`);
-    } catch (e) {
-      if (e.message.includes("UNIQUE constraint failed")) {
-        console.warn(`The short URL "${u}" already exists in the database.`);
-      } else {
-        console.error("Error during operation:", e);
-        throw e;
-      }
-    }
-    return id;
-  }
-  async toDeleteFromDb({ id }) {
-    let app = this;
-    let n = 0;
-    try {
-      let stmt = app.db.prepare(`
+			let result = stmt.run(url, u);
+			id = result.lastInsertRowid;
+			console.log(`db id: ${id}`);
+		} catch (e) {
+			if (e.message.includes("UNIQUE constraint failed")) {
+				console.warn(`The short URL "${u}" already exists in the database.`);
+			} else {
+				console.error("Error during operation:", e);
+				throw e;
+			}
+		}
+		return id;
+	}
+	async toDeleteFromDb({ id }) {
+		let app = this;
+		let n = 0;
+		try {
+			let stmt = app.db.prepare(`
         DELETE FROM urls 
         WHERE id = ?
       `);
-      
-      let result = stmt.run(id);
-      n = result.changes;
-      
-      if (n > 0) {
-        console.log(`Successfully deleted record with id: ${id}`);
-      } else {
-        console.log(`No record found with id: ${id}`);
-      }
-    } catch (e) {
-      console.error("Error during delete operation:", e);
-      throw e;
-    }
-    return n;
-  }
+
+			let result = stmt.run(id);
+			n = result.changes;
+
+			if (n > 0) {
+				console.log(`Successfully deleted record with id: ${id}`);
+			} else {
+				console.log(`No record found with id: ${id}`);
+			}
+		} catch (e) {
+			console.error("Error during delete operation:", e);
+			throw e;
+		}
+		return n;
+	}
 	async toShortenUrl({ url, shortid, analytics, filterbots, verbose }) {
 		let app = this;
 		let { client } = app;
@@ -221,145 +227,159 @@ class App extends cutil.mixin(AppBase, infoer, dumper, pathable, iwdbApp) {
 				verbose,
 			});
 			console.log(u);
-      await app.toAddToDb({ url, u });
+			await app.toAddToDb({ url, u });
 		} catch (e) {
 			console.log(e.message);
 			process.exit(1);
 		}
 	}
-  async toAdd({ u, url }) {
-    let app = this;
-    try {
-      await app.toAddToDb({ url, u });
-    } catch (e) {
-      console.log(e.message);
-      process.exit(1);
-    }
-  }
-  async toDelete({ id }) {
-    let app = this;
-    try {
-      await toDeleteFromDb({ id });
-    } catch (e) {
-      console.log(e.message);
-      process.exit(1);
-    }
-  }
-  display(item) {
-    let app = this;
-    console.log([
-      cutil.asString(item.id).padStart(7),
-      cutil.df(new Date(item.dt + "Z")).padStart(23),
-      cutil.asString(item.u).padEnd(31),
-      item.url,
-    ].join(" "));
-  }
-  async toList({ oldest, newest }) {
-    let app = this;
-    try {
-      let sql = "SELECT * FROM urls";
-      if (oldest) {
-        sql += " ORDER BY dt ASC";
-      } else if (newest) {
-        sql += " ORDER BY dt DESC";
-      }
-      let items = app.db.prepare(sql).all();
-      console.log(`Items found: ${items.length}`);
-      for (let item of items) {
-        app.display(item);
-      }
-    } catch (e) {
-      console.log(e.message);
-      process.exit(1);
-    }
-  }
-  async toSearch({ query }) {
-    let app = this;
-    try {
-      let sql = "SELECT * FROM urls WHERE url LIKE ? OR u LIKE ?";
-      query = `%${query}%`;
-      let items = app.db.prepare(sql).all(query, query);
-      console.log(`Items found: ${items.length}`);
-      for (let item of items) {
-        app.display(item);
-      }
-    } catch (e) {
+	async toAdd({ u, url }) {
+		let app = this;
+		try {
+			await app.toAddToDb({ url, u });
+		} catch (e) {
 			console.log(e.message);
 			process.exit(1);
 		}
-  }
-  async toUpdate({ id }) {
-    let app = this;
-    try {
-      let item = app.db.prepare("SELECT * FROM urls WHERE id = ?").get(id);
-      
-      if (cutil.a(item)) {
-        app.display(item);
-        let { dt, url, u } = item;
-        let s;
+	}
+	async toDelete({ id }) {
+		let app = this;
+		try {
+			await toDeleteFromDb({ id });
+		} catch (e) {
+			console.log(e.message);
+			process.exit(1);
+		}
+	}
+	display(item) {
+		let app = this;
+		console.log([
+			cutil.asString(item.id).padStart(7),
+			cutil.df(new Date(item.dt + "Z")).padStart(23),
+			cutil.asString(item.u).padEnd(31),
+			item.url,
+		].join(" "));
+	}
+	async toList({ oldest, newest }) {
+		let app = this;
+		try {
+			let sql = "SELECT * FROM urls";
+			if (oldest) {
+				sql += " ORDER BY dt ASC";
+			} else if (newest) {
+				sql += " ORDER BY dt DESC";
+			}
+			let items = app.db.prepare(sql).all();
+			console.log(`Items found: ${items.length}`);
+			for (let item of items) {
+				app.display(item);
+			}
+		} catch (e) {
+			console.log(e.message);
+			process.exit(1);
+		}
+	}
+	async toSearch({ query }) {
+		let app = this;
+		try {
+			let sql = "SELECT * FROM urls WHERE url LIKE ? OR u LIKE ?";
+			query = `%${query}%`;
+			let items = app.db.prepare(sql).all(query, query);
+			console.log(`Items found: ${items.length}`);
+			for (let item of items) {
+				app.display(item);
+			}
+		} catch (e) {
+			console.log(e.message);
+			process.exit(1);
+		}
+	}
+	async toUpdate({ id }) {
+		let app = this;
+		try {
+			let item = app.db.prepare("SELECT * FROM urls WHERE id = ?").get(id);
 
-        if ((s = await Inputter.toInput(`dt (${cutil.df(new Date(item.dt + "Z"))})?`))) {
-          dt = new Date(s).toISOString().replace("T", " ").slice(0, 19);
-        }
+			if (cutil.a(item)) {
+				app.display(item);
+				let { dt, url, u } = item;
+				let s;
 
-        if ((s = await Inputter.toInput(`url (${url})?`))) {
-          url = s;
-        }
+				if (
+					(s = await Inputter.toInput(
+						`dt (${cutil.df(new Date(item.dt + "Z"))})?`,
+					))
+				) {
+					dt = new Date(s).toISOString().replace("T", " ").slice(0, 19);
+				}
 
-        if ((s = await Inputter.toInput(`u (${u})?`))) {
-          u = s;
-        }
+				if ((s = await Inputter.toInput(`url (${url})?`))) {
+					url = s;
+				}
 
-        app.display({ id, dt, url, u });
+				if ((s = await Inputter.toInput(`u (${u})?`))) {
+					u = s;
+				}
 
-        try {
-          app.db.prepare(`
+				app.display({ id, dt, url, u });
+
+				try {
+					app.db.prepare(`
             UPDATE urls 
             SET dt = ?, url = ?, u = ? 
             WHERE id = ?
           `).run(dt, url, u, id);
-          
-          console.log(`Record ${id} updated successfully.`);
-        } catch (e) {
-          console.error("Failed to update record.");
-          throw e;
-        }
-      } else {
-        console.log(`Record with id ${id} not found.`);
-      }
-    } catch (e) {
+
+					console.log(`Record ${id} updated successfully.`);
+				} catch (e) {
+					console.error("Failed to update record.");
+					throw e;
+				}
+			} else {
+				console.log(`Record with id ${id} not found.`);
+			}
+		} catch (e) {
 			console.log(e.message);
 			process.exit(1);
 		}
-  }
-  async toExport({ file }) {
+	}
+	async toExport({ file }) {
+		let app = this;
+		try {
+			let items = app.db.prepare("SELECT * FROM urls ORDER BY dt ASC").all();
+			await fs.writeFile(file, JSON.stringify(items, null, 2), "utf8");
+			console.log(`Successfully exported ${items.length} records to ${file}`);
+		} catch (e) {
+			console.error("Export failed:", e.message);
+		}
+	}
+  async toImport({ file, merge }) {
     let app = this;
     try {
-      let items = app.db.prepare("SELECT * FROM urls ORDER BY dt DESC").all();
-      await fs.writeFile(file, JSON.stringify(items, null, 2), "utf8");
-      console.log(`Successfully exported ${items.length} records to ${file}`);
-    } catch (e) {
-      console.error("Export failed:", e.message);
-    }
-  }
-  async toImport({ file }) {
-    let app = this;
-    try {
-      let content = await fs.readFile(file, "utf8");
-      let items = JSON.parse(content);
+      const content = await fs.readFile(file, "utf8");
+      const items = JSON.parse(content);
       
-      // Using OR IGNORE in case you have the UNIQUE constraint on 'u'
-      let stmt = app.db.prepare(`
-        INSERT OR IGNORE INTO urls (id, dt, url, u) 
-        VALUES (@id, @dt, @url, @u)
-      `);
+      let sql = merge 
+        ? `INSERT OR IGNORE INTO urls (dt, url, u) VALUES (@dt, @url, @u)`
+        : `INSERT OR IGNORE INTO urls (id, dt, url, u) VALUES (@id, @dt, @url, @u)`;
 
-      const insertMany = app.db.transaction((data) => {
-        for (const item of data) stmt.run(item);
-      });
+      const stmt = app.db.prepare(sql);
 
-      insertMany(items);
-      console.log(`Imported records from ${file}. (Duplicates skipped)`);
+      // Manual Transaction
+      try {
+        app.db.prepare("BEGIN TRANSACTION").run();
+
+        for (const item of items) {
+          stmt.run(item);
+        }
+
+        app.db.prepare("COMMIT").run();
+        console.log(`Import completed successfully (${items.length} records processed).`);
+      } catch (err) {
+        // If something goes wrong, try to rollback to keep the DB clean
+        app.db.prepare("ROLLBACK").run();
+        throw err;
+      }
+      
     } catch (e) {
       console.error("Import failed:", e.message);
     }
